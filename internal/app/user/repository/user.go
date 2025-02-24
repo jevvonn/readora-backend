d0cb943055c172405933c5b432ebee29a91532b9
jevvonn <darmawanb985@gmail.com>
2025-02-24 22:50:05 +0700

    feat: update user repository to include user retrieval and creation methods

--- internal/app/user/repository/user.go
+++ internal/app/user/repository/user.go
@@ -1,15 +1,24 @@
 package repository
 
-import(
-    "gorm.io/gorm"
+import (
+	"github.com/jevvonn/reodora-backend/internal/domain/entity"
+	"gorm.io/gorm"
 )
 
-type UserPostgreSQLItf interface {}
+type UserPostgreSQLItf interface{}
 
 type UserPostgreSQL struct {
-    db *gorm.DB
+	db *gorm.DB
 }
 
 func NewUserPostgreSQL(db *gorm.DB) UserPostgreSQLItf {
-    return &UserPostgreSQL{db}
+	return &UserPostgreSQL{db}
+}
+
+func (r *UserPostgreSQL) GetSpecificUser(user entity.User) error {
+	return r.db.First(&user).Error
+}
+
+func (r *UserPostgreSQL) CreateUser(user entity.User) error {
+	return r.db.Create(&user).Error
 }
