d6ac9109ca096242d5493d21fc78a38f720a4b16
jevvonn <darmawanb985@gmail.com>
2025-02-25 02:28:50 +0700

    feat: update user entity to use uniqueIndex for username and email fields

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -9,8 +9,8 @@ import (
 type User struct {
 	ID            uuid.UUID `gorm:"primaryKey" json:"id"`
 	Name          string    `gorm:"type:varchar(255);not null" json:"name"`
-	Username      string    `gorm:"type:varchar(255);not null;unique" json:"username"`
-	Email         string    `gorm:"type:varchar(255);not null;unique" json:"email"`
+	Username      string    `gorm:"type:varchar(255);uniqueIndex;not null;unique" json:"username"`
+	Email         string    `gorm:"type:varchar(255);uniqueIndex;not null;unique" json:"email"`
 	Password      string    `gorm:"type:varchar(255);not null;" json:"password"`
 	Role          string    `gorm:"type:userRole;not null;default:'USER'" json:"role"`
 	EmailVerified bool      `gorm:"type:boolean;not null" json:"email_verified"`
