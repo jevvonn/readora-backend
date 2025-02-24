d92d89b3be529071e8a04718a327f42027ed8f90
jevvonn <darmawanb985@gmail.com>
2025-02-24 22:49:46 +0700

    feat: update logger to write logs to daily files instead of stdout

--- internal/infra/logger/logger.go
+++ internal/infra/logger/logger.go
@@ -1,7 +1,9 @@
 package logger
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -10,8 +12,12 @@ func New() *log.Logger {
 	logger := log.New()
 
 	logger.SetFormatter(&log.JSONFormatter{})
-	logger.SetOutput(os.Stdout)
 	logger.SetLevel(log.TraceLevel)
+
+	todayDate := time.Now().Format("2006-01-02")
+	path := fmt.Sprintf("logs/%s.log", todayDate)
+	file, _ := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logger.SetOutput(file)
 
 	return logger
 }
