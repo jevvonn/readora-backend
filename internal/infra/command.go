5eee6f17b1bf49fe10444d917db9cd0b83c61b03
jevvonn <darmawanb985@gmail.com>
2025-02-25 00:08:07 +0700

    refactor: move command handler for database migration and seeding to

---
+++ internal/infra/command.go
@@ -0,0 +1,28 @@
+package infra
+
+import (
+	"flag"
+	"os"
+
+	"github.com/jevvonn/reodora-backend/internal/infra/postgresql"
+	"gorm.io/gorm"
+)
+
+func CommandHandler(db *gorm.DB) {
+	var migrationCmd string
+	var seederCmd bool
+
+	flag.StringVar(&migrationCmd, "m", "", "Migrate database 'up' or 'down'")
+	flag.BoolVar(&seederCmd, "s", false, "Seed database")
+	flag.Parse()
+
+	if migrationCmd != "" {
+		postgresql.Migrate(db, migrationCmd)
+		os.Exit(0)
+	}
+
+	if seederCmd {
+		postgresql.Seed(db)
+		os.Exit(0)
+	}
+}
